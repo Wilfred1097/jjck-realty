872e8dac6da4111b331519d87671c7ce7d2ddda6
Wil Fred Catalan <catalanwilfredo97@gmail.com>
2024-01-05 10:52:44 +0800

    updated server.js

--- server/server.js
+++ server/server.js
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 
 // Create a MySQL connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'jjck',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
